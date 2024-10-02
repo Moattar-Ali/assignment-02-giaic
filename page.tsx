d4bc674a07fb2f37e140ad06792948cb566f5696
Moattar Ali <164565363+Moattar-Ali@users.noreply.github.com>
2024-10-02 22:24:41 +0500

    Add files via upload

---
+++ page.tsx
@@ -0,0 +1,36 @@
+//ASSIGNMENT:02:
+
+export default function Home() {
+  return (
+    <div className="container">
+      <div className="description">
+      <h1 id="myName"><b><i><u>MOATTAR ALI</u></i></b></h1>
+      <br></br>
+      <h3><b>PERSONAL INFORMATION:</b></h3>
+        <p>Residential Address: Orangi Town, Karachi</p>
+        <p>Email address: moattar789@gmail.com</p>
+        <br></br>
+        <h3><b>EDUCATION:</b></h3>
+        <ul>
+            <li>Matriculation <b>"Science"</b> from Islamia English Model School from BSEK.</li>
+            <li>Intermediate <b>"Science"</b> from Sir Syed Govt Girls College from BIEK.</li>
+            <li><b>"BS in Physics"</b> continuing from NED university, Karachi.</li>
+        </ul>
+<br></br>
+            <h4><b>COMPUTER SKILLS:</b></h4>
+            <ul>
+            <li>HTML</li>
+            <li>CSS</li>
+            <li>JAVASCRIPT</li>
+            <li>TYPESCRIPT</li>
+            <li>C LANGUAGE</li>
+            <li>MS OFFICE</li>
+             </ul>
+
+      </div>
+    <div className="myImage">
+
+    </div>
+    </div>
+  );
+}
